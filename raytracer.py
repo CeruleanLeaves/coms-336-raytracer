e76d246a88c334edd14d522610aff14e245b6439
Matthew He <hematthew2378@gmail.com>
2025-11-27 23:47:11 -0600

    made gradiant image

--- raytracer.py
+++ raytracer.py
@@ -5,3 +5,23 @@ def main():
 
     width = 400
     height = 225
+
+    image = np.zeros(shape=(height, width, 3), dtype=np.float32)
+
+    for y in range(height):
+        for x in range(width):
+            r = x / (width - 1)
+            g = y / (height - 1)
+            b = .5
+            image[y, x, 0] = r
+            image[y, x, 1] = g
+            image[y, x, 2] = b
+
+    image_uint8 = (np.clip(image, 0.0, 1.0) * 255).astype(np.uint8)
+
+    rendered_image = Image.fromarray(image_uint8, mode='RGB')
+    rendered_image.save('output.png')
+    print('yayy')
+
+if __name__ == '__main__':
+    main()
